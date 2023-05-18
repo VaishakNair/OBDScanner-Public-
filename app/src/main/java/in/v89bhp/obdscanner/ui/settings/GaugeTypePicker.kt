6301b31443cf604bad202a150e0bec75a69b2921
VaishakNair <vaishak.k.nair@gmail.com>
2023-05-18 12:48:08 +0530

    Start logic to add top app bar to GaugeTypePicker composable.

--- app/src/main/java/in/v89bhp/obdscanner/ui/settings/GaugeTypePicker.kt
+++ app/src/main/java/in/v89bhp/obdscanner/ui/settings/GaugeTypePicker.kt
@@ -1,15 +1,53 @@
 package `in`.v89bhp.obdscanner.ui.settings
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.viewinterop.AndroidViewBinding
+import `in`.v89bhp.obdscanner.R
 import `in`.v89bhp.obdscanner.databinding.GaugeTypePickerFragmentLayoutBinding
+import kotlinx.coroutines.launch
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GaugeTypePicker(modifier: Modifier = Modifier) {
-    AndroidViewBinding(GaugeTypePickerFragmentLayoutBinding::inflate) {
+    Scaffold(topBar = {
+        TopAppBar(
+            title = {
+                Text(stringResource(id = R.string.app_name))
+            },
+            actions = {
+
+            },
+            navigationIcon = {
+                IconButton(onClick = {
+                    // TODO
+                    with(drawerState) {
+                        if (isOpen) scope.launch { close() }
+                        else scope.launch { open() }
+                    }
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.M,
+                        contentDescription = "Back"
+                    )
+                }
+            })
+    }) { contentPadding ->
+        AndroidViewBinding(modifier = Modifier.padding(contentPadding),
+            factory = GaugeTypePickerFragmentLayoutBinding::inflate) {
 //        val myFragment = fragmentContainerView. // TODO Find instance to the fragment.
 
+        }
     }
 }
