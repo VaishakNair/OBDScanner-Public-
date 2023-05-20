f77c061c4fab2b263db34ac6201a799646ba1c65
VaishakNair <vaishak.k.nair@gmail.com>
2023-05-20 11:36:18 +0530

    Continue bluetooth connection logic.

--- app/src/main/java/in/v89bhp/obdscanner/ui/connectivity/BluetoothConnection.kt
+++ app/src/main/java/in/v89bhp/obdscanner/ui/connectivity/BluetoothConnection.kt
@@ -1,5 +1,6 @@
 package `in`.v89bhp.obdscanner.ui.connectivity
 
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothDevice
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.Arrangement
@@ -16,8 +17,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import `in`.v89bhp.obdscanner.helpers.BluetoothHelper
 
 @Composable
 fun BluetoothConnection(
@@ -31,31 +34,47 @@ fun BluetoothConnection(
 
 @Composable
 fun CircularProgress(text: String, modifier: Modifier = Modifier) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally,
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-    modifier = modifier.fillMaxSize()) {
+        modifier = modifier.fillMaxSize()
+    ) {
         CircularProgressIndicator()
-        Text(text = text,
-        modifier = Modifier.padding(top = 16.dp))
+        Text(
+            text = text,
+            modifier = Modifier.padding(top = 16.dp)
+        )
     }
 }
 
+@SuppressLint("MissingPermission")
 @Composable
-fun PairedDevices(pairedDevices: List<BluetoothDevicz>, modifier: Modifier = Modifier) {
+fun PairedDevices(pairedDevices: List<BluetoothDevice>, modifier: Modifier = Modifier) {
+    val bluetoothSocket = BluetoothHelper.socket
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(8.dp),
         modifier = modifier.padding(16.dp)
     ) {
-      items(pairedDevices, key = {it.address}) {
-        Column(modifier = Modifier.fillMaxWidth()) {
-            Text(text = it.name,
-            )
-            Text(text = "Connected",
-            modifier = Modifier.padding(top = 8.dp))
-            Divider(modifier = Modifier.padding(top = 8.dp))
-        }
+        items(pairedDevices, key = { it.address }) { bluetoothDevice ->
+            val connected =
+                bluetoothSocket?.let { it.isConnected && it.remoteDevice.name == bluetoothDevice!!.name }
+                    ?: false
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Text(
+                    text = bluetoothDevice.name,
 
-      }
+                    )
+                Text(
+                    text = if (connected) "Connected" else "Disconnected",
+                    color = colorResource(
+                        id = if (connected) android.R.color.holo_green_light else
+                            android.R.color.holo_red_light
+                    ),
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+                Divider(modifier = Modifier.padding(top = 8.dp))
+            }
+        }
     }
 }
 
@@ -69,7 +88,7 @@ fun CircularProgressPreview() {
 @Preview(showBackground = true)
 @Composable
 fun PairedDevicesPreview() {
-    PairedDevices(listOf(BluetoothDevicz("1", "boAt"), BluetoothDevicz("2", "Jabra")))
+//    PairedDevices(listOf(BluetoothDevicz("1", "boAt"), BluetoothDevicz("2", "Jabra")))
 }
 
 data class BluetoothDevicz(val address: String, val name: String)
