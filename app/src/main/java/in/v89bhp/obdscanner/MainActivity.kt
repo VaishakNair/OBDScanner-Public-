6303be754a143b6eb04425a9d1d266539434d9d3
VaishakNair <vaishak.k.nair@gmail.com>
2023-05-24 13:13:35 +0530

    Check for bluetooth enabled status on onResume callback of MainActivity.

--- app/src/main/java/in/v89bhp/obdscanner/MainActivity.kt
+++ app/src/main/java/in/v89bhp/obdscanner/MainActivity.kt
@@ -2,6 +2,7 @@ package `in`.v89bhp.obdscanner
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -9,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.fragment.app.FragmentActivity
 import `in`.v89bhp.obdscanner.helpers.BluetoothHelper
 import `in`.v89bhp.obdscanner.helpers.ElmHelper
+import `in`.v89bhp.obdscanner.ui.connectivity.BluetoothConnectionViewModel
 import `in`.v89bhp.obdscanner.ui.theme.OBDScannerTheme
 
 class MainActivity : FragmentActivity() {
@@ -18,6 +20,7 @@ class MainActivity : FragmentActivity() {
         const val EXTRA_DESTINATION_ID = "extra_dest_id"
     }
 
+    private val viewModel by viewModels<BluetoothConnectionViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -35,5 +38,10 @@ class MainActivity : FragmentActivity() {
                 }
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.updateBtEnabledStatus()
     }
 }
