9d26fe7ccddab53449f46a39ea189c637216844d
VaishakNair <vaishak.k.nair@gmail.com>
2023-05-24 11:49:07 +0530

    Continue wiring bluetooth connection logic.

--- app/src/main/java/in/v89bhp/obdscanner/MainActivity.kt
+++ app/src/main/java/in/v89bhp/obdscanner/MainActivity.kt
@@ -1,14 +1,7 @@
 package `in`.v89bhp.obdscanner
 
-import android.annotation.SuppressLint
-import android.bluetooth.BluetoothAdapter
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -16,7 +9,6 @@ import androidx.compose.ui.Modifier
 import androidx.fragment.app.FragmentActivity
 import `in`.v89bhp.obdscanner.helpers.BluetoothHelper
 import `in`.v89bhp.obdscanner.helpers.ElmHelper
-import `in`.v89bhp.obdscanner.ui.connectivity.BluetoothConnectionViewModel
 import `in`.v89bhp.obdscanner.ui.theme.OBDScannerTheme
 
 class MainActivity : FragmentActivity() {
@@ -25,8 +17,6 @@ class MainActivity : FragmentActivity() {
         const val TAG = "MainActivity"
         const val EXTRA_DESTINATION_ID = "extra_dest_id"
     }
-
-    private val bluetoothConnectionViewModel by viewModels<BluetoothConnectionViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,24 +36,4 @@ class MainActivity : FragmentActivity() {
             }
         }
     }
-
-//    override fun onResume() {
-//        super.onResume()
-//        applicationContext.registerReceiver(btStateChangedReceiver, IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED))
-//    }
-//
-//    override fun onPause() {
-//        super.onPause()
-//        applicationContext?.unregisterReceiver(btStateChangedReceiver)
-//    }
-//
-//    private val btStateChangedReceiver = object : BroadcastReceiver() {
-//        override fun onReceive(p0: Context?, intent: Intent?) {
-//            bluetoothConnectionViewModel.updateBtEnabledStatus()
-//        }
-//    }
-
-
-
-
 }
