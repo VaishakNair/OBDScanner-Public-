bf19c0cbaca0dc8d78db744858f10043133b846b
VaishakNair <vaishak.k.nair@gmail.com>
2023-05-31 11:35:49 +0530

    Continue wiring connectivity banner logic.

--- app/src/main/java/in/v89bhp/obdscanner/OBDScannerAppViewModel.kt
+++ app/src/main/java/in/v89bhp/obdscanner/OBDScannerAppViewModel.kt
@@ -11,9 +11,7 @@ import android.os.Handler
 import android.os.Looper
 import android.os.Message
 import android.util.Log
-import androidx.annotation.StringRes
 import androidx.compose.runtime.*
-import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.AndroidViewModel
 import androidx.preference.PreferenceManager
 import `in`.v89bhp.obdscanner.helpers.BluetoothHelper
@@ -53,20 +51,22 @@ class OBDScannerAppViewModel(application: Application) : AndroidViewModel(applic
 
     @SuppressLint("MissingPermission")
     private fun establishLastConnection() {
-        if(!BluetoothHelper.connecting) {
+        if (!BluetoothHelper.connecting) {
             val (isLastConnectedDeviceAvailable, device) = isLastConnectedDeviceAvailable()
-            if(isLastConnectedDeviceAvailable) {
+            if (isLastConnectedDeviceAvailable) {
                 // Try connecting to last connected device. Don't ask for PIN.
                 lastConnectedDevice = device as BluetoothDevice
                 showConnectivityHeader = true
-                connectivityHeaderMessage = (getApplication() as Context).getString(R.string.connecting_to, device!!.name)
+                connectivityHeaderMessage =
+                    (getApplication() as Context).getString(R.string.connecting_to, device!!.name)
                 connectivityHeaderBackground = ConnectivityYellow
                 showConnectingSnackbar = true
                 BluetoothHelper.connect(bluetoothConnectionHandler, device)
             } else {
                 // Display 'You are offline' header
                 showConnectivityHeader = true
-                connectivityHeaderMessage = (getApplication() as Context).getString(R.string.offline)
+                connectivityHeaderMessage =
+                    (getApplication() as Context).getString(R.string.offline)
                 connectivityHeaderBackground = ConnectivityYellow
 
             }
@@ -75,7 +75,8 @@ class OBDScannerAppViewModel(application: Application) : AndroidViewModel(applic
 
     @SuppressLint("MissingPermission")
     private fun isLastConnectedDeviceAvailable(): Pair<Boolean, BluetoothDevice?> {
-        val deviceName = PreferenceManager.getDefaultSharedPreferences(getApplication()).getString("deviceName", null)
+        val deviceName = PreferenceManager.getDefaultSharedPreferences(getApplication())
+            .getString("deviceName", null)
         return deviceName?.let {
             BluetoothHelper.queryPairedDevices()?.let {
                 val bluetoothDevice = it.find {
@@ -95,49 +96,63 @@ class OBDScannerAppViewModel(application: Application) : AndroidViewModel(applic
         @SuppressLint("MissingPermission")
         override fun handleMessage(msg: Message) {
 //            if(!isDestroyed) { // TODO Check if this is needed or not
-                when(msg?.arg1) {
-                    0 -> {// Connection failed. Retry till user cancellation:
-                        if(!stopTrying) {
-                            Log.i(TAG, "Retrying connection...")
-                            BluetoothHelper.connect(this, lastConnectedDevice)
-                        } else {
-                            Log.i(TAG, "Stopping trying...")
-                            stopTrying = false
-                        }
-                    }
-
-                    1 -> {// Connection established
-                        // Dismiss the 'Connecting to' snackbar
-                        showConnectingSnackbar = false
-                        // Display green 'Connected to $pairedDeviceName' header
-                        showConnectivityHeader = true
-                        connectivityHeaderMessage = (getApplication() as Context).getString(R.string.connected_to, lastConnectedDevice!!.name) // TODO Wire logic to
-                        // hide connectivity banner after 5 secs
-                            connectivityHeaderBackground = ConnectivityGreen
-//                        showConnectivityHeader(getString(R.string.connected_to, lastConnectedDevice!!.name), R.color.connectivity_green, 5000)
+            when (msg?.arg1) {
+                0 -> {// Connection failed. Retry till user cancellation:
+                    if (!stopTrying) {
+                        Log.i(TAG, "Retrying connection...")
+                        BluetoothHelper.connect(this, lastConnectedDevice)
+                    } else {
+                        Log.i(TAG, "Stopping trying...")
+                        stopTrying = false
                     }
                 }
+
+                1 -> {// Connection established
+                    // Dismiss the 'Connecting to' snackbar
+                    showConnectingSnackbar = false
+                    // Display green 'Connected to $pairedDeviceName' header
+                    showConnectivityHeader = true
+                    connectivityHeaderMessage = (getApplication() as Context).getString(
+                        R.string.connected_to,
+                        lastConnectedDevice!!.name
+                    ) // TODO Wire logic to
+                    // hide connectivity banner after 5 secs
+                    connectivityHeaderBackground = ConnectivityGreen
+//                        showConnectivityHeader(getString(R.string.connected_to, lastConnectedDevice!!.name), R.color.connectivity_green, 5000)
+                }
+            }
 //            }
         }
     }
 
     val bluetoothConnectionStateChangeReceiver = object : BroadcastReceiver() {
+        @SuppressLint("MissingPermission")
         override fun onReceive(context: Context?, intent: Intent?) {
             val bluetoothConnectionState = intent?.action
-            val bluetoothDevice = intent?.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+            val bluetoothDevice =
+                intent?.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
 
-            when(bluetoothConnectionState)  {
+            when (bluetoothConnectionState) {
                 BluetoothDevice.ACTION_ACL_DISCONNECTED -> {
                     // Bluetooth connection lost. Try connecting to the last connected device.
                     establishLastConnection()
                 }
+
                 BluetoothDevice.ACTION_ACL_CONNECTED -> {
                     // Display green 'Connected to $pairedDeviceName' header
-                    showConnectivityHeader(getString(R.string.connected_to, bluetoothDevice!!.name), R.color.connectivity_green, 5000)
+
+                    showConnectivityHeader = true
+                    connectivityHeaderMessage = (getApplication() as Context).getString(
+                        R.string.connected_to,
+                        bluetoothDevice!!.name
+                    )
+                    connectivityHeaderBackground = ConnectivityGreen // TODO Wire logic to
+                    // hide connectivity banner after 5 secs
+
 
                     // Store name of connected device to default shared preferences file
-                    getPreferences(Context.MODE_PRIVATE).edit().putString("deviceName", bluetoothDevice.name)
-                        .apply()
+                    PreferenceManager.getDefaultSharedPreferences(getApplication()).edit()
+                        .putString("deviceName", bluetoothDevice.name).apply()
                 }
             }
         }
