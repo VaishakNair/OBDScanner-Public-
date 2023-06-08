fc75a4af3c0a16c2b7e9bd4e47193762ff7469e9
VaishakNair <vaishak.k.nair@gmail.com>
2023-06-08 12:49:18 +0530

    Start wiring search logic to gauge picker.

--- app/src/main/java/in/v89bhp/obdscanner/ui/gauges/GaugePicker.kt
+++ app/src/main/java/in/v89bhp/obdscanner/ui/gauges/GaugePicker.kt
@@ -1,25 +1,30 @@
 package `in`.v89bhp.obdscanner.ui.gauges
 
-import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidViewBinding
-import androidx.fragment.app.findFragment
 import `in`.v89bhp.obdscanner.R
 import `in`.v89bhp.obdscanner.databinding.GaugePickerFragmentLayoutBinding
-import `in`.v89bhp.obdscanner.databinding.GaugeTypePickerFragmentLayoutBinding
-import `in`.v89bhp.obdscanner.fragments.GaugePickerFragment
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -34,6 +39,7 @@ fun GaugePicker(
             },
             actions = {
 
+
             },
             navigationIcon = {
                 IconButton(onClick = {
@@ -46,8 +52,31 @@ fun GaugePicker(
                 }
             })
     }) { contentPadding ->
-        AndroidViewBinding(modifier = Modifier.padding(contentPadding),
-            factory = GaugePickerFragmentLayoutBinding::inflate) {
+        Column(modifier = modifier.padding(contentPadding)) {
+            Row(
+                modifier = modifier
+                    .padding(8.dp)
+                    .background(MaterialTheme.colorScheme.secondaryContainer, CircleShape),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = stringResource(id = R.string.search),
+                    modifier = Modifier.padding(start = 16.dp),
+                )
+                TextField(
+                    text = "Search",
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(16.dp),
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+
+            }
+            AndroidViewBinding(
+                factory = GaugePickerFragmentLayoutBinding::inflate
+            ) {
+            }
         }
     }
 
