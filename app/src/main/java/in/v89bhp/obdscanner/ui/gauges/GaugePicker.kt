4c8917edc625753077220b79e0756889919ead4b
VaishakNair <vaishak.k.nair@gmail.com>
2023-06-08 13:06:05 +0530

    Finish wiring search logic for gauge picker.

--- app/src/main/java/in/v89bhp/obdscanner/ui/gauges/GaugePicker.kt
+++ app/src/main/java/in/v89bhp/obdscanner/ui/gauges/GaugePicker.kt
@@ -2,7 +2,7 @@ package `in`.v89bhp.obdscanner.ui.gauges
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
@@ -18,7 +18,6 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -53,34 +52,29 @@ fun GaugePicker(
             })
     }) { contentPadding ->
         Column(modifier = modifier.padding(contentPadding)) {
-            Row(
+
+            TextField(
                 modifier = modifier
                     .padding(8.dp)
+                    .fillMaxWidth()
                     .background(MaterialTheme.colorScheme.secondaryContainer, CircleShape),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Search,
-                    contentDescription = stringResource(id = R.string.search),
-                    modifier = Modifier.padding(start = 16.dp),
-                )
-                TextField(
-                    value = GaugesAppBarState.searchTextFieldValue,
-                    onValueChange = {
-                        GaugesAppBarState.searchTextFieldValue = it
-                        GaugesAppBarState.filterPids()
-                    },
-                    label = { Text("Label") }
-                )
-//                Text(
-//                    text = "Search",
-//                    modifier = Modifier
-//                        .weight(1f)
-//                        .padding(16.dp),
-//                    style = MaterialTheme.typography.bodyMedium,
-//                )
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = stringResource(id = R.string.search),
 
-            }
+                    )
+                },
+                value = GaugesAppBarState.searchTextFieldValue,
+                onValueChange = {
+                    GaugesAppBarState.searchTextFieldValue = it
+                    GaugesAppBarState.filterPids()
+                },
+                label = { Text(stringResource(R.string.search)) },
+                singleLine = true
+            )
+
+
             AndroidViewBinding(
                 factory = GaugePickerFragmentLayoutBinding::inflate
             ) {
