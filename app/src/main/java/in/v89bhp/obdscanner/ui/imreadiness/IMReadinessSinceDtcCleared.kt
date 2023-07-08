33b300ccecbefd032fe4b043196e29085a9a8050
VaishakNair <vaishak.k.nair@gmail.com>
2023-07-08 11:56:10 +0530

    Continue...

--- app/src/main/java/in/v89bhp/obdscanner/ui/imreadiness/IMReadinessSinceDtcCleared.kt
+++ app/src/main/java/in/v89bhp/obdscanner/ui/imreadiness/IMReadinessSinceDtcCleared.kt
@@ -1,20 +1,21 @@
 package `in`.v89bhp.obdscanner.ui.imreadiness
 
 
-
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.viewinterop.AndroidViewBinding
 import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.NavBackStackEntry
 import `in`.v89bhp.obdscanner.R
-import `in`.v89bhp.obdscanner.databinding.ImReadinessDtcListItemBinding
 import `in`.v89bhp.obdscanner.ui.connectivity.CircularProgress
 import `in`.v89bhp.obdscanner.ui.connectivity.ErrorCard
+import `in`.v89bhp.obdscanner.ui.theme.ConnectivityGreen
+import `in`.v89bhp.obdscanner.ui.theme.HoloRedLight
 
 @Composable
 fun IMReadinessSinceDtcCleared(
@@ -26,18 +27,24 @@ fun IMReadinessSinceDtcCleared(
     lifecycleOwner: LifecycleOwner = backStackEntry
 ) {
 
-    if(viewModel.loading) {
+    if (viewModel.loading) {
         CircularProgress(text = stringResource(R.string.loading))
-    } else if(viewModel.isError) {
-        ErrorCard(errorMessage = stringResource(R.string.try_again), onClick = { viewModel.loadMonitorStatuses() })
+    } else if (viewModel.isError) {
+        ErrorCard(
+            errorMessage = stringResource(R.string.try_again),
+            onClick = { viewModel.loadMonitorStatuses() })
     } else {
-       
-            LazyColumn {
-                items(viewModel.monitorStatuses) {
-                    Column() {
-                        
-                    }
+
+        LazyColumn {
+            items(viewModel.monitorStatuses) {
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Text(text = it.monitorName)
+                    Text(
+                        text = it.status,
+                        color = if (it.status == stringResource(id = R.string.complete)) ConnectivityGreen else HoloRedLight
+                    )
                 }
+            }
         }
     }
 
